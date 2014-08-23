43af251a66cace35f370adf694dec0a7cac19241
ouertani <ouertani@gmail.com>
2014-08-23 16:36:12 +0300

    Adding tck test class

--- src/test/java/io/vertx/test/ext/reactivestreams/tck/PublisherVerificationTest.java
+++ src/test/java/io/vertx/test/ext/reactivestreams/tck/PublisherVerificationTest.java
@@ -2,7 +2,9 @@ package io.vertx.test.ext.reactivestreams.tck;
 
 import io.vertx.core.buffer.Buffer;
 import io.vertx.test.ext.reactivestreams.MyPublisher;
+import io.vertx.test.ext.reactivestreams.MySubscription;
 import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
 import org.reactivestreams.tck.PublisherVerification;
 import org.reactivestreams.tck.TestEnvironment;
 
@@ -23,6 +25,7 @@ public class PublisherVerificationTest extends PublisherVerification<Buffer> {
 
     @Override
     public Publisher<Buffer> createErrorStatePublisher() {
-        return MyPublisher publisher = new MyPublisher();;
+        return subscriber -> subscriber.onError(new RuntimeException("Can't subscribe subcriber: " + subscriber + ", because of reasons."));
+
     }
 }
